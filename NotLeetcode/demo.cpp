75ab4bef2736f3b6de9a7786f2041969005c3652
Yash Parmar <yashparmar1027@gmail.com>
2025-09-05 23:27:11 +0530

    Binary to decimal conversion

--- NotLeetcode/demo.cpp
+++ NotLeetcode/demo.cpp
@@ -1,11 +1,3 @@
 #include<iostream>
 using namespace std;
 
-int prime(int n){
-
-}
-int main(){
-    int n=5;
-    int num = prime(n);
-    return 0;
-}
---
+++ decimaltoBinary.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int decToBinary(int decNum){
+    int ans=0,pow=1;
+    while(decNum>0){
+        int rem= decNum%2;
+        decNum /=2;
+
+        ans += (rem*pow);
+        pow *=10;
+    }
+    return ans;
+}
+
+int bintoDecimal(int binNum){
+    int ans=0,pow=1;
+    while(binNum>0){
+        int rem= binNum%10;
+        binNum /=10;
+
+        ans= rem*pow;
+        pow *=2;
+    }
+    return ans;
+}
+int main(){
+    int decNum=4;
+    for(int i=0;i<10;i++)
+    {
+        cout<<decToBinary(i)<<endl;
+    }
+
+    cout<<endl;
+    cout<<bintoDecimal(100);
+    return 0;
+}
