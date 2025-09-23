b8f29fef32e6faf2934c69a7d0368f6dee84d245
Yash Parmar <yashparmar1027@gmail.com>
2025-09-23 18:57:32 +0530

    Checking for .exe file

---
+++ NotLeetcode/Patterns/p2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void pattern1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=i;j--){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
+
+void pattern2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=i;j--){
+            cout<<j+1<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void pattern3(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=i;j--){
+            cout<<i+1<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+
+int main(){
+    int t;
+    cin>>t;      // for test case enter: how many pattern want to print
+    for(int i=0;i<t;i++){
+       int n;
+       cin>>n;        // no of rows for each pattern particular
+       pattern1(n);          
+       pattern2(n);
+       pattern3(n);
+
+    }
+    
+    return 0;
+}
