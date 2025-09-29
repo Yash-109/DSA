087c1edf6d80d35f4f71c8899ce0dcdec803b7dd
Yash Parmar <yashparmar1027@gmail.com>
2025-09-29 19:37:09 +0530

    Completed patterns

--- Patterns/p8.cpp
+++ Patterns/p8.cpp
@@ -25,20 +25,33 @@ void ptn1(int n)
     }
 }
 
+// 4444444
+// 4333334
+// 4322234
+// 4321234
+// 4322234
+// 4333334
+// 4444444
 void ptn2(int n)
 {
     for(int i=0;i<2*n-1;i++)
     {
         for(int j=0;j<2*n-1;j++)
         {
-
+            int top=i;
+            int left = j;
+            int right= (2*n-2)- j;
+            int down = (2*n-2) - i;
+            cout<<(n - min(min(top,down), min(right,left)));
         }
+        cout<<endl;
     }
 }
 int main()
 {
     int n;
     cin>>n;
-    ptn1(n);
+    // ptn1(n);
+    ptn2(n);
     return 0;
 }
