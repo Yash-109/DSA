c7da55dc672e480c5d8e3b5736dfe17b7228f1ef
Yash Parmar <yashparmar1027@gmail.com>
2025-10-03 21:23:21 +0530

    Learning array vector

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
- main()
+void reverse(vector<int> &vec, int size)
 {
+    int start = 0;
+    int end = size - 1;
+    
+    while(start < end)
+    {
+        swap(vec[start], vec[end]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    vector<int> vec= {1, 2, 3, 4, 5, 6,7};
+    int size = 7;
+    
+    cout << "Original vecay: ";
+    for(int i = 0; i < size; i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+    
+    reverse(vec, size);
+    
+    cout << "Reversed vectpray: ";
+    for(int i = 0; i < size; i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
