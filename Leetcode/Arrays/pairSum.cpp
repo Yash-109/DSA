761dc57eb81e36409a9835b549c6289fdbe5a59b
Yash Parmar <yashparmar1027@gmail.com>
2025-10-04 16:46:32 +0530

    Two sum

---
+++ Leetcode/Arrays/pairSum.cpp
@@ -0,0 +1,60 @@
+//Problem: 1. Two Sum
+//Difficulty: Easy
+//Link: https://leetcode.com/problems/two-sum/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+vector<int> pairsum(vector<int> nums,int target)
+{
+    // brute force approach o(n^2)
+    // vector<int> ans;
+    // int n=nums.size();
+    // for(int i=0;i<n;i++){
+    //     for(int j=i+1;j<n;j++)
+    //     {
+    //         if(nums[i]+nums[j]== target)
+    //         {
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             return ans;
+    //         }
+    //     }
+    // }
+    // return ans;
+
+    //two pointer o(n)  ony if we have sorted array
+
+    vector<int> ans;
+    int n=nums.size();
+    int i=0;
+    int j=n-1;
+    while(i<j)
+    {
+        int pairsum=nums[i]+nums[j];
+        if(pairsum>target)
+        {
+            j--;
+        }
+        else if(pairsum<target)
+        {
+            i++;
+        }
+        else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> nums={2,7,11,15};
+    int target=18;
+    vector<int> ans=pairsum(nums,target);
+    cout<<ans[0]<<","<<ans[1];
+    return 0;
+}
