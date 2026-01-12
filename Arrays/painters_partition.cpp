fb63369b4c8bb109d47709ca3bca3aac5581e95a
Yash Parmar <yashparmar1027@gmail.com>
2026-01-12 11:35:06 +0530

    Painters Partition

--- Arrays/painters_partition.cpp
+++ Arrays/painters_partition.cpp
@@ -1,5 +1,84 @@
 // Painter’s Partition Problem
 
+//          """ same as Vook Alloation Problem"""
 // Given are N boards of length of each given in the form of array, and M painters, such that each painter takes 1 unit of time to paint 1 unit of the board.
 
 // The task is to find the minimum time to paint all boards under the constraints that any painter will only paint continuous sections of boards.
+
+// if arr = [40,30,10,20] and m = 2(no. of painters)
+// n = 4 (no. of boards)
+
+// then possible case :  p1=40, p2=60   max = 60
+//                       p1=70, p2=30   max = 70 
+//                       p1=80, p2=20   max = 80
+
+//         we need minimum time to paint so ans is first case: 60
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isValid(vector<int> arr,int n,int m,int maxAllowedMinutes)
+{
+    int painter=1, minutes=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i] > maxAllowedMinutes)  // edge case
+        {
+            return false;
+        }
+        if(minutes + arr[i] <= maxAllowedMinutes)
+        {
+            minutes+=arr[i];
+        }
+        else
+        {
+            painter++;
+            minutes=arr[i];
+        }
+    }
+    return painter > m ? false:true;
+}
+int allocatePainter(vector<int> arr,int n, int m)
+{
+    if(m>n)  // edge case
+    {
+        return -1;
+    }
+
+    int time=0;
+    for(int i=0;i<n;i++)  
+    {
+        time+=arr[i];   // total possible time
+    }
+
+    int ans=0;
+    int st=0,end=time;
+
+    while(st<=end)  // binary search logic
+    {
+        int mid = st + (end-st)/2;
+
+        if(isValid(arr,n,m,mid))
+        {
+            ans = mid;
+            end = mid-1;
+        }
+        else
+        {
+            st = mid + 1;
+        }
+    }
+    return ans;
+}       
+int main()
+{
+    vector<int> arr={40,30,10,20};
+    int n=arr.size();
+    int  m= 2;
+
+    cout<<allocatePainter(arr,n,m);
+
+    return 0;
+}
