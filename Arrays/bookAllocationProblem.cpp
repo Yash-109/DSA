cfc4e85e19d2ef261cd671d65c090b90799e8c1b
Yash Parmar <yashparmar1027@gmail.com>
2026-01-12 11:06:23 +0530

    .

--- Arrays/bookAllocationProblem.cpp
+++ Arrays/bookAllocationProblem.cpp
@@ -48,14 +48,14 @@ int allocateBooks(vector<int> &arr,int n,int m) // TOtal tc of code: O(logN * n)
         return -1;  // if student>no.of books
     }
     int sum=0;
-    for(int i = 0;i<n;i++)  // O(n)
+    for(int i = 0;i<n;i++)  // O(n)   
     {
         sum+=arr[i];
     }
 
     int ans=0;
     int st=0, end=sum; // rangle of possible ans which is 0 to 10
- 
+
     while(st<=end)   // O(logN)*O(n) = o(n*logN)
     {
         int mid = st + (end-st)/2;
@@ -83,3 +83,27 @@ int main()
     cout<<allocateBooks(arr,n,m);
     return 0;
 }
+
+
+// arr = [15, 17, 20], N = 3        M = 2
+
+// sum : 52
+// search space : 0 to 52
+// mid = 26        ...Invalid
+
+// search space : 27 to 52
+// mid = 39        ...Valid
+
+// search space : 27 to 38
+// mid = 32        ...Valid
+
+// search space : 27 to 31
+// mid = 29        ...Invalid
+
+// search space : 30 to 31
+// mid = 30        ...Invalid
+
+// search space : 31 to 31
+// mid = 31        ...Invalid
+
+// 32 is the ans
