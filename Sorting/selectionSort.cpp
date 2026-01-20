197da6cdd5a7f4bdb215d037d521972e63316f64
Yash Parmar <yashparmar1027@gmail.com>
2026-01-20 21:16:41 +0530

    selection sort

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -25,6 +25,24 @@ void bubbleSort(int arr[], int n)
     }
 }
 
+void selectionSort(int arr[],int n)  // O(n^2)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int smallestIdx=i;  // starting index of unsorted part
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[smallestIdx])
+            {
+                smallestIdx=j;
+            }
+        }
+
+        swap(arr[i],arr[smallestIdx]);
+    }
+}
+
 void printArray(int arr[], int n)
 {
     for(int i = 0; i < n; i++) 
@@ -37,8 +55,10 @@ int main()
 {
     int arr[]={4,1,5,2,3};
     int n=sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr,n);
-    printArray(arr,n);
+    // bubbleSort(arr,n);
+    // printArray(arr,n);
 
+    selectionSort(arr,n);
+    printArray(arr,n);
     return 0;
 }
