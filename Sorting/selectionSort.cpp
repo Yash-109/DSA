19f74eb8faadb50ab68477b9574f60fa09a729de
Yash Parmar <yashparmar1027@gmail.com>
2026-01-20 21:41:29 +0530

    insertion sort

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -2,29 +2,6 @@
 #include<iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n)
-{
-    // Outer loop: iterate through all elements
-    for(int i = 0; i < n - 1; i++)
-    {
-        bool isSwap = false;
-        // Inner loop: compare adjacent elements and swap if needed
-        for(int j = 0; j < n - i - 1; j++)
-        {
-            if(arr[j]>arr[j+1])
-            {
-                swap(arr[j] , arr[j+1]); 
-                isSwap=true;
-            }
-        }
-
-        if(isSwap == false)  // optimization but still tc: O(n^2)
-        {
-            break;
-        }
-    }
-}
-
 void selectionSort(int arr[],int n)  // O(n^2)
 {
     for(int i=0;i<n-1;i++)
@@ -55,8 +32,6 @@ int main()
 {
     int arr[]={4,1,5,2,3};
     int n=sizeof(arr) / sizeof(arr[0]);
-    // bubbleSort(arr,n);
-    // printArray(arr,n);
 
     selectionSort(arr,n);
     printArray(arr,n);
