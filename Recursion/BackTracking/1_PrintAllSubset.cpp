5cc63df3891fba694ab772b468487a84ff1ad956
Yash <yashparmar1027@gmail.com>
2026-02-11 20:17:35 +0530

    .

--- Recursion/BackTracking/1_PrintAllSubset.cpp
+++ Recursion/BackTracking/1_PrintAllSubset.cpp
@@ -42,3 +42,37 @@ int main() {
     // Output: All 2^3 = 8 subsets including empty set
     return 0;
 }
+
+
+// leetcode problem: 78:  same time complexity ans all but returning same array insted of printing it 
+class Solution {
+public:
+    void getAllSubsets(
+        vector<int>& nums,
+        vector<int>& ans,
+        int i,
+        vector<vector<int>>& allSubsets
+    ) {
+        if (i == nums.size()) {
+            allSubsets.push_back(ans);
+            return;
+        }
+
+        // include
+        ans.push_back(nums[i]);
+        getAllSubsets(nums, ans, i + 1, allSubsets);
+
+        ans.pop_back();
+
+        // exclude
+        getAllSubsets(nums, ans, i + 1, allSubsets);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+
+        getAllSubsets(nums, ans, 0, allSubsets);
+        return allSubsets;
+    }
+};
