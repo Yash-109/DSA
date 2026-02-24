3c9d25834a29d3fddeea65fb538ed0e30736f71d
Yash <yashparmar1027@gmail.com>
2026-02-24 20:50:50 +0530

    .

--- OOPS/5_ShallowDeepCopy.cpp
+++ OOPS/5_ShallowDeepCopy.cpp
@@ -1,12 +1,10 @@
 /*
+
 Shallow & Deep Copy:
 
 A shallow copy of an object copies all of the member values from one object to another.
 A deep copy, on the other hand, not only copies the member values but also makes copies of
 any dynamically allocated memory that the members point to.
-
-
-
 */
 
 #include<iostream>
